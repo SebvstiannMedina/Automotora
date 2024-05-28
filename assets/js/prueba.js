929f86f70f047567c01554ecf24c1c5efd62d1ef
SebvstiannMedina <seba.medina@duocuc.cl>
2024-05-28 16:40:45 -0400

    usuarios y admin listos

--- assets/js/prueba.js
+++ assets/js/prueba.js
@@ -1,17 +1,43 @@
-
-
-let btnEnviar = document.getElementById("btnEnviar");
-
-let email = "";
-let password = "";
-
-btnEnviar.addEventListener("click", (e)=> {
-
-    e.preventDefault();
-    email = document.getElementById("email").value;
-    password = document.getElementById("password").value;
-
-    console.log(email);
-    console.log(password);
-
-})
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar que el formulario se envíe por defecto
+  
+    // Obtener los valores del formulario
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var role = document.getElementById('role').value;
+  
+    // Definir arrays de correos electrónicos y contraseñas para administradores y clientes
+    var administradores = [
+      { email: "seba.medina@duocuc.cl", password: "seba1234" },
+      { email: "admin2@example.com", password: "admin2pass" }
+    ];
+    var clientes = [
+      { email: "cliente1@example.com", password: "cliente1pass" },
+      { email: "cliente2@example.com", password: "cliente2pass" }
+    ];
+  
+    // Verificar si el correo y la contraseña coinciden con algún usuario
+    var usuarioEncontrado;
+    if (role === 'admin') {
+      usuarioEncontrado = administradores.find(function(usuario) {
+        return usuario.email === email && usuario.password === password;
+      });
+    } else {
+      usuarioEncontrado = clientes.find(function(usuario) {
+        return usuario.email === email && usuario.password === password;
+      });
+    }
+  
+    // Redirigir según el rol y la autenticación
+    if (usuarioEncontrado) {
+      if (role === 'admin') {
+        window.location.href = 'admin.html';
+      } else {
+        window.location.href = 'cliente.html';
+      }
+    } else {
+      // Si el usuario no existe o las credenciales son incorrectas, mostrar un mensaje de error
+      alert("Correo o contraseña incorrectos");
+    }
+  });
+  
